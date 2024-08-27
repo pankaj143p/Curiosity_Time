45c1031b13c95c7519b508459b70f48bcde285fd
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-08-27 12:15:58 +0530

    navbar

---
+++ curiosity-time/app/constants/index.ts
@@ -0,0 +1,24 @@
+'use-client'
+
+export const navMenuData = [
+    {
+        route : '/',
+        label : 'Home',
+        icon : 'icons/home.svg'
+    },
+    {
+        route : 'discover',
+        label : 'Discover',
+        icon : 'icons/discover.svg'
+    },
+    {
+        route : '/createPodcast',
+        label : 'Create New Podcasts', 
+        icon : 'icons/home.svg'
+    },
+    {
+      route : '/profile',
+      label : 'Profile',
+      icon : '/icons/profile.svg'
+    }
+]
--- curiosity-time/components/leftSidebar.tsx
+++ curiosity-time/components/leftSidebar.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import Image from 'next/image'
 import React from 'react';
 import logo from '../public/icons/mainlogo.png'
+import { navMenuData } from '@/app/constants';
+
 
 const LeftSidebar = () => {
   return (
@@ -12,8 +14,13 @@ const LeftSidebar = () => {
         <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
           <Image src={logo} alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">Curiosity Time</h1>
-        </Link>
-         
+          </Link>
+           {navMenuData.map(({route,label,icon})=>{
+            return <Link href={route} key={label} className='flex gap-3'>
+            <Image src={icon} alt={''} width={20} height={20} ></Image>
+            {label}
+            </Link>
+           })}
         </nav>
       </section>
     </div>
