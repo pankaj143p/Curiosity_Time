51d8ae11609b3fc96d25395fcebcead0c382c538
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-08-30 22:18:57 +0530

    bugs fix

--- curiosity-time/convex/_generated/dataModel.ts
+++ curiosity-time/convex/_generated/dataModel.ts
@@ -16,7 +16,7 @@ import type {
     SystemTableNames,
   } from "convex/server";
   import type { GenericId } from "convex/values";
-  import schema from "../schema.js";
+  import schema from "./schema";
   
   /**
    * The names of all of your Convex tables.
