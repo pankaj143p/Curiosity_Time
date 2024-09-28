c011657cb89e05b940c343ad9bc29a148cb20166
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-09-28 17:57:17 +0530

    convex setup

--- curiosity-time/app/(root)/page.tsx
+++ curiosity-time/app/(root)/page.tsx
@@ -1,14 +1,21 @@
-'use-client'
+"use client";
 import PodcastCard from '@/components/cards/podcastCard';
-import { Button } from '@/components/ui/button';
 import { podcastData } from '@/constants';
 import React from 'react';
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className='ml-9 mt-14 text-purple-400'>
     <section className='flex flex-col gap-5'>
      <h1 className='text-20 font-bold text-white-1'>tranding podcasts</h1> 
+     <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+      {tasks?.map(({ _id, text }) => <div className=' text-white-1' key={_id}>{text}</div>)}
+    </div>
      <div className='podcast_grid'>
        {podcastData.map(({ id, title, description, imgURL }) => (
       <PodcastCard
